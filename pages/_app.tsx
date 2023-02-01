3b0275570840c541fff887cad1c0a3a99982f171
CamiloRX777 <juancamilogallego70@gmail.com>
2023-02-01 10:57:16 -0700

    chore: remove useless dependecies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,7 @@ import { createClient, configureChains, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { SessionProvider } from "next-auth/react";
 import { mainnet, goerli } from "wagmi/chains";
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from "recoil";
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -27,9 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={client}>
       <SessionProvider session={pageProps.session} refetchInterval={0}>
-        <RecoilRoot>
           <Component {...pageProps} />
-        </RecoilRoot>
       </SessionProvider>
     </WagmiConfig>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,6 @@ export default function Home() {
         <Container>
           <Welcomepage />
         </Container>
-        <Footer />
       </main>
     </>
   );
