0d1713a8a4aa49a90b77fe76836cb1b69fa1f1da
CamiloRX777 <juancamilogallego70@gmail.com>
2023-04-16 14:06:21 -0700

    hotfix: add imagekit loader

--- components/ui/ImageLoader.tsx
+++ components/ui/ImageLoader.tsx
@@ -3,8 +3,16 @@
 
 import { ImageLoader } from "next/image";
 
-export const CustomLocalLoader: ImageLoader = ({ src, width, quality }) => {
-    return `https://ik.imagekit.io/fg4geoqzrz${src}?w=${width}&q=${quality || 75}`;
+const CustomLocalLoader = ({ src, width, quality }) => {
+  if(src[0] === "/") src = src.slice(1);
+  const params = [`w-${width}`];
+  if (quality) {
+    params.push(`q-${quality}`);
+  }
+  const paramsString = params.join(",");
+  var urlEndpoint = "https://ik.imagekit.io/your_imagekit_id";
+  if(urlEndpoint[urlEndpoint.length-1] === "/") urlEndpoint = urlEndpoint.substring(0, urlEndpoint.length - 1);
+  return `${urlEndpoint}/${src}?tr=${paramsString}`
 }
 
 export const CustomLoaderNFT: ImageLoader = ({ src, width, quality }) => {
