f0f0b2f681d0ad458a8fcf2aa31ec1b26dca80c3
CamiloRX777 <juancamilogallego70@gmail.com>
2023-04-16 14:15:33 -0700

    hotfix: add imagekit loader

--- components/ui/ImageLoader.tsx
+++ components/ui/ImageLoader.tsx
@@ -10,7 +10,7 @@ export const CustomLocalLoader = ({ src, width, quality }) => {
     params.push(`q-${quality}`);
   }
   const paramsString = params.join(",");
-  var urlEndpoint = "https://ik.imagekit.io/your_imagekit_id";
+  var urlEndpoint = "https://ik.imagekit.io/fg4geoqzrz";
   if(urlEndpoint[urlEndpoint.length-1] === "/") urlEndpoint = urlEndpoint.substring(0, urlEndpoint.length - 1);
   return `${urlEndpoint}/${src}?tr=${paramsString}`
 }
